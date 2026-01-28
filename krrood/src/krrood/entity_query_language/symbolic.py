7ed591046cb33fc6ad3ad743a9fad7935c51db97
jonas Dech <j.dech@gmx.net>
2026-01-28 21:39:32 +0100

    Added first version of condition

--- krrood/src/krrood/entity_query_language/symbolic.py
+++ krrood/src/krrood/entity_query_language/symbolic.py
@@ -467,6 +467,7 @@ class ResultProcessor(CanBehaveLikeAVariable[T], ABC):
         Evaluate the query and map the results to the correct output data structure.
         This is the exposed evaluation method for users.
         """
+        print("here")
         SymbolGraph().remove_dead_instances()
         yield from map(self._child_._process_result_, self._evaluate__())
 
@@ -501,6 +502,61 @@ class ResultProcessor(CanBehaveLikeAVariable[T], ABC):
             edge_style=edge_style,
             label_max_chars_per_line=label_max_chars_per_line,
         )
+
+
+@dataclass
+class Condition(SymbolicExpression):
+    conditions: tuple[ConditionType, ...]
+
+    def __init__(self, *conditions: ConditionType):
+        super().__init__()
+        self.conditions = conditions
+        self.create_symbolic_expressions(conditions)
+
+    def create_symbolic_expressions(
+        self, conditions: tuple[SymbolicExpression, ...]
+    ) -> Iterable[SymbolicExpression]:
+        condition_list = list(conditions)
+
+        # If there are no conditions raise error.
+        if len(condition_list) == 0:
+            raise ValueError("No conditions provided")
+
+        # If there's a constant condition raise error.
+        literal_expressions = [
+            exp for exp in condition_list if not isinstance(exp, SymbolicExpression)
+        ]
+        if literal_expressions:
+            raise LiteralConditionError(literal_expressions)
+
+        # Build the expression from the conditions
+        expression = (
+            chained_logic(AND, *condition_list)
+            if len(condition_list) > 1
+            else condition_list[0]
+        )
+
+        # set the child of the query object descriptor to the expression and return self.
+        self._update_child_(expression)
+        return expression
+
+    def _evaluate__(
+        self,
+        sources: Optional[Dict[int, Any]] = None,
+        parent: Optional[SymbolicExpression] = None,
+    ) -> Iterable[OperationResult]:
+        for res in self._root_._evaluate__(sources, parent):
+            yield res
+
+    @property
+    def _name_(self) -> str:
+        return "Condition"
+
+    def _all_variable_instances_(self) -> List[Variable]:
+        pass
+
+    def evaluate(self):
+        return list(self._child_._evaluate__())
 
 
 @dataclass(eq=False, repr=False)
@@ -712,6 +768,9 @@ class ResultQuantifier(ResultProcessor[T], ABC):
         sources: Optional[Dict[int, Any]] = None,
         parent: Optional[SymbolicExpression] = None,
     ) -> Iterable[T]:
+        print(f"here evaluate, {self.__class__}")
+        print(f"sources: {sources}")
+        print(f"parent: {parent}")
         sources = sources or {}
         self._eval_parent_ = parent
         if self._id_ in sources:
