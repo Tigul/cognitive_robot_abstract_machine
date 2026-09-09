24575e4830f0f8347832a298cca50f08e5c04aa2
jonas Dech <j.dech@gmx.net>
2026-09-09 22:58:48 +0200

    Removed build in open from transport action

--- test/coraplex_test/test_designator/test_multi_robot_action_designator.py
+++ test/coraplex_test/test_designator/test_multi_robot_action_designator.py
@@ -40,6 +40,7 @@ from coraplex.robot_plans.actions.core.pick_up import (
     PickUpAction,
 )
 from coraplex.robot_plans.actions.core.placing import PlaceAction
+from coraplex.robot_plans.plan_transformations import OpenDrawerBeforePickUp
 from coraplex.robot_plans.actions.core.robot_body import (
     MoveTorsoAction,
     SetGripperAction,
@@ -806,6 +807,7 @@ def test_transport_open_container(mutable_multiple_robot_apartment, rclpy_node):
 
     if isinstance(robot, HSRB):
         return
+    context.plan_transformations.append(OpenDrawerBeforePickUp())
     description = TransportAction(
         object_designator=world.get_semantic_annotations_by_type(Spoon)[0],
         target_location=Pose.from_xyz_rpy(
